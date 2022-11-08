a5a405f410165f8be04b5e93ac2a9bc3413069ff
Roland Szilágyi <75616702+roland-szilagyi@users.noreply.github.com>
2022-11-08 21:23:24 +0100

    test

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-document.querySelector('.js-btn1')
-    .addEventListener('click', btn1Play)
+// ----- BUTTONS ----- //
 
 function btn1Play() {
-    const btn1Sound = new Audio('./snare.wav');
+    const btn1Sound = new Audio('./src/audio/kick.wav');
     btn1Sound.play();
 }
 
+function btn2Play() {
+    const btn2Sound = new Audio('./src/audio/hat.wav');
+    btn2Sound.play();
+}
+
+function btn3Play() {
+    const btn3Sound = new Audio('./src/audio/perc.wav');
+    btn3Sound.play();
+}
+
+// ----- EVENT LISTENERS ----- //
+
+document.querySelector('.js-btn1')
+    .addEventListener('onclick', btn1Play);
+
+document.querySelector('.js-btn2')
+    .addEventListener('click', btn2Play);
+
+document.querySelector('.js-btn3')
+    .addEventListener('click', btn3Play);
