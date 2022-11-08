86b744fab9a5bd5dfc277a652b56a8fdc65297d3
Roland Szilágyi <75616702+roland-szilagyi@users.noreply.github.com>
2022-11-08 22:16:41 +0100

    btn-test2

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-// ----- BUTTONS ----- //
-
-function btn1Play() {
-    const btn1Sound = new Audio('./src/audio/kick.wav');
-    btn1Sound.play();
-}
-
-function btn2Play() {
-    const btn2Sound = new Audio('./src/audio/hat.wav');
-    btn2Sound.play();
-}
-
-function btn3Play() {
-    const btn3Sound = new Audio('./src/audio/perc.wav');
-    btn3Sound.play();
-}
-
 // ----- EVENT LISTENERS ----- //
-
 document.querySelector('.js-btn1')
-    .addEventListener('onclick', btn1Play);
+    .addEventListener('mousedown', btn1);
 
 document.querySelector('.js-btn2')
-    .addEventListener('click', btn2Play);
+    .addEventListener('mousedown', btn2);
 
 document.querySelector('.js-btn3')
-    .addEventListener('click', btn3Play);
+    .addEventListener('mousedown', btn3);
+
+// ----- BUTTONS ----- //
+
+function btn1() {
+    const btn1Sound = new Audio('./src/audio/kick.wav');
+    btn1Sound.play()
+}
+
+function btn2() {
+    const btn2Sound = new Audio('./src/audio/hat.wav');
+    btn2Sound.play()
+}
+
+function btn3() {
+    const btn3Sound = new Audio('./src/audio/perc.wav');
+    btn3Sound.play()
+}
